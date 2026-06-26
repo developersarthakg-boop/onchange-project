324b497a248de9f06587ee832544057a40c70c19
Sarthak Tripathi <developersarthakg@gmail.com>
2026-06-27 02:24:22 +0530

    Onchange changes

---
+++ src/Onchanges.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+function Onchanges() {
+  const [name, setName] = useState("");
+
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+  return (
+    <div>
+      <p> The learning start with the basics of onchange </p>
+      <input
+        type="text"
+        placeholder="Enter your name"
+        value={name}
+        onChange={handleChange}
+      />
+      <button onClick={() => setName("")}>Clear</button>
+      <p>Hello , {name}</p>
+      <button>Submit</button>
+    </div>
+  );
+}
+
+export default Onchanges;
